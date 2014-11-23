882db330e4aeacee3e5293e4b85e0062a6a16d01
Gerson Kurz <gerson.kurz@gmail.com>
2014-11-23 15:09:40 +0100

    This is really shit: scrollviewre inside flipview doesn't zoom correct, outside doesn't control...

--- viehstall/MainPage.xaml.cs
+++ viehstall/MainPage.xaml.cs
@@ -10,6 +10,8 @@ using System.Diagnostics;
 using Windows.Storage.Pickers;
 using Windows.UI.Xaml.Media.Imaging;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Input;
 
 namespace viehstall
 {
@@ -23,15 +25,48 @@ namespace viehstall
             this.Loaded += MainPage_Loaded;
         }
 
+        private T FindFirstElementInVisualTree<T>(DependencyObject parentElement) where T : DependencyObject
+        {
+            if (parentElement != null)
+            {
+                var count = VisualTreeHelper.GetChildrenCount(parentElement);
+                if (count == 0)
+                    return null;
+
+                for (int i = 0; i < count; i++)
+                {
+                    var child = VisualTreeHelper.GetChild(parentElement, i);
+
+                    if (child != null && child is T)
+                        return (T)child;
+                    else
+                    {
+                        var result = FindFirstElementInVisualTree<T>(child);
+                        if (result != null)
+                        {
+                            return result;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
         private async void FlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             await MyPictureCache.GoTo(MyFlipView.SelectedIndex);
+            // thanks, http://stackoverflow.com/questions/14667556/a-simple-photo-album-with-pinch-and-zoom-using-flipview
+            var flipViewItem = MyFlipView.ContainerFromIndex(MyFlipView.SelectedIndex);
+            ResizeImageToFitScreen(FindFirstElementInVisualTree<ScrollViewer>(flipViewItem));
         }
 
         private async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            await MyPictureCache.SwitchToFolder(@"C:\Users\Gerson\Pictures\Wallpapers");
+            await MyPictureCache.SwitchToFolder(@"C:\Users\Gerson\Pictures\Test\1");
             MyFlipView.ItemsSource = MyPictureCache.ListOfPictures;
+            var flipViewItem = MyFlipView.ContainerFromIndex(MyFlipView.SelectedIndex);
+            ResizeImageToFitScreen(FindFirstElementInVisualTree<ScrollViewer>(flipViewItem));
+
         }
         
 /*
@@ -90,6 +125,55 @@ namespace viehstall
 
             var folder = await folderPicker.PickSingleFolderAsync();
             await MyPictureCache.SwitchToFolder(folder);
+        }
+
+        private void ResizeImageToFitScreen(ScrollViewer scroll)
+        {
+            if (scroll != null)
+            {
+                var period = TimeSpan.FromMilliseconds(10);
+                float imageWidth = PictureInfo.LastKnownPictureInfo.PictureInMemory.PixelWidth;
+                float imageHeight = PictureInfo.LastKnownPictureInfo.PictureInMemory.PixelHeight;
+
+                float ScreenWidth = 0.90f * (float)this.ActualWidth;
+                float ScreenHeight = 0.90f * (float)this.ActualHeight;
+
+                float factor = 1.0f;
+                if (ScreenWidth > ScreenHeight)
+                {
+                    if (imageWidth > ScreenWidth)
+                        factor = ScreenWidth / imageWidth;
+                    else
+                        factor = 0.95f;
+                }
+                else
+                {
+
+                    if (imageHeight > ScreenHeight)
+                        factor = ScreenHeight / imageHeight;
+                    else
+                        factor = 0.95f;
+                }
+                Debug.WriteLine("ZF {0} calculated", factor);
+
+                Windows.System.Threading.ThreadPoolTimer.CreateTimer(async (source) =>
+                {
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        var Succes = scroll.ChangeView(null, null, factor, true);
+                    });
+                }, period);
+            }
+        }
+
+        void scroll_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Debug.WriteLine("scroll: size changed");
+        }
+
+        private void ScrollViewer_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            ResizeImageToFitScreen(sender as ScrollViewer);
         }
     }
 }
